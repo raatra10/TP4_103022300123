1b05eea276be3bc7448e625699a91cd0c87e76a4
raatra10 <rafi180350@gmail.com>
2025-03-10 22:27:21 +0700

    menambahkan implementasi dengan state-based construction

--- tpmodul4_103022300123/Program.cs
+++ tpmodul4_103022300123/Program.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Masukkan nama kelurahan:");
+//        string kelurahan = Console.ReadLine();
+
+//        string kodePos = KodePos.GetKodePos(kelurahan);
+//        Console.WriteLine($"Kode pos {kelurahan}: {kodePos}");
+
+//        Console.ReadLine();
+//    }
+//}
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Masukkan nama kelurahan:");
-        string kelurahan = Console.ReadLine();
+        DoorMachine door = new DoorMachine();
 
-        string kodePos = KodePos.GetKodePos(kelurahan);
-        Console.WriteLine($"Kode pos {kelurahan}: {kodePos}");
+        Console.WriteLine("\nTes membuka pintu:");
+        door.BukaPintu();
+
+        Console.WriteLine("\nTes mengunci pintu:");
+        door.KunciPintu();
+
+        Console.WriteLine("\nTes mengunci lagi:");
+        door.KunciPintu();
+
+        Console.ReadLine();
     }
 }
 
